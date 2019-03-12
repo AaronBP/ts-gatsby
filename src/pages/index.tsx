64ae03ab3fd9edd83b02ee06694e857f59a57031
Aaron Phillips <developingaaron@gmail.com>
2019-03-12 12:19:23 +0000

    test gatsby deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h1>Hi people</h1>
+        <h1>Hi people testing</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
         <Link to="/page-2/">Go to page 2</Link>
